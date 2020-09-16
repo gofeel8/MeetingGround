e1dd17309c57b89be1b8cda5c127bbe9cb772b8d
박진용 <jypark5826@gmail.com>
2020-09-16 14:40:18 +0900

    [Android/JYP] 방만들기에서 뒤로가면 튕기는 버그 수정

--- AndroidStudioProjects/MeetingRound/app/src/main/java/com/mgmg/meetinground/MainActivity.java
+++ AndroidStudioProjects/MeetingRound/app/src/main/java/com/mgmg/meetinground/MainActivity.java
@@ -8,7 +8,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
 import com.kakao.auth.ISessionCallback;
 import com.kakao.auth.Session;
