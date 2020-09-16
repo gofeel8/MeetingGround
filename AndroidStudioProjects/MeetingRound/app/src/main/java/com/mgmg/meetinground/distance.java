a8e4384928d66089b327f4f05ab6cd002c22adf0
unknown <kimmydkemf@naver.com>
2020-09-16 17:22:13 +0900

    [Android/LSH] 지도부분추가

---
+++ AndroidStudioProjects/MeetingRound/app/src/main/java/com/mgmg/meetinground/distance.java
@@ -0,0 +1,26 @@
+package com.mgmg.meetinground;
+
+public class distance {
+    private String name;
+    private double distance;
+    public distance(String name, double distance) {
+        this.name = name;
+        this.distance = distance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+}
