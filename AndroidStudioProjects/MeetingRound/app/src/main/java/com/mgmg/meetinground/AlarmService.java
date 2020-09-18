3f432f83963afda2b25dabb661b060d046ee89e1
philsang <gofeel8@hanmail.net>
2020-09-18 17:58:52 +0900

    [Android/GPS]모임시간알람설정수정

--- AndroidStudioProjects/MeetingRound/app/src/main/java/com/mgmg/meetinground/AlarmService.java
+++ AndroidStudioProjects/MeetingRound/app/src/main/java/com/mgmg/meetinground/AlarmService.java
@@ -2,6 +2,7 @@ package com.mgmg.meetinground;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.ActivityManager;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
@@ -10,12 +11,16 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.Configuration;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Binder;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.os.IBinder;
+import android.os.Looper;
 import android.util.Log;
 import android.widget.RemoteViews;
 import android.widget.Toast;
@@ -25,12 +30,20 @@ import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -39,107 +52,324 @@ import java.util.TimerTask;
 
 public class AlarmService extends Service {
 
-    private LocationRequest mLocationRequest;
-    private GoogleApiClient mGoogleApiClient;
-    private Location mLastLocation;
+    private static final String PACKAGE_NAME =
+            "com.mgmg.meetinground;";
 
-    private static int UPDATE_INTERVER = 5000;
-    private static int FASTEST_INTERVER = 3000;
-    private static int DISTANCE = 10;
+    private static final String TAG = AlarmService.class.getSimpleName();
+
+    /**
+     * The name of the channel for notifications.
+     */
+    private static final String CHANNEL_ID = "channel_01";
+
+    static final String ACTION_BROADCAST = PACKAGE_NAME + ".broadcast";
+
+    static final String EXTRA_LOCATION = PACKAGE_NAME + ".location";
+    private static final String EXTRA_STARTED_FROM_NOTIFICATION = PACKAGE_NAME +
+            ".started_from_notification";
+
+    private final IBinder mBinder = new LocalBinder();
+
+    /**
+     * The desired interval for location updates. Inexact. Updates may be more or less frequent.
+     */
+    private static final long UPDATE_INTERVAL_IN_MILLISECONDS = 10000;
+
+    /**
+     * The fastest rate for active location updates. Updates will never be more frequent
+     * than this value.
+     */
+    private static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS =
+            UPDATE_INTERVAL_IN_MILLISECONDS / 2;
+
+    /**
+     * The identifier for the notification displayed for the foreground service.
+     */
+    private static final int NOTIFICATION_ID = 12345678;
+
+    /**
+     * Used to check whether the bound activity has really gone away and not unbound as part of an
+     * orientation change. We create a foreground service notification only if the former takes
+     * place.
+     */
+    private boolean mChangingConfiguration = false;
+
+    private NotificationManager mNotificationManager;
+
+    /**
+     * Contains parameters used by {@link com.google.android.gms.location.FusedLocationProviderApi}.
+     */
+    private LocationRequest mLocationRequest;
+
+    /**
+     * Provides access to the Fused Location Provider API.
+     */
+    private FusedLocationProviderClient mFusedLocationClient;
+
+    /**
+     * Callback for changes in location.
+     */
+    private LocationCallback mLocationCallback;
+
+    private Handler mServiceHandler;
+
+    /**
+     * The current location.
+     */
+    private Location mLocation;
 
     DatabaseReference userRef;
 
-    private Location lastKnownLocation = null;
 
-
-    @Nullable
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+    public AlarmService() {
     }
 
+    @Override
+    public void onCreate() {
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                onNewLocation(locationResult.getLastLocation());
+            }
+        };
+
+        createLocationRequest();
+        getLastLocation();
+
+        HandlerThread handlerThread = new HandlerThread(TAG);
+        handlerThread.start();
+        mServiceHandler = new Handler(handlerThread.getLooper());
+        mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        // Android O requires a Notification Channel.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.app_name);
+            // Create the channel for the notification
+            NotificationChannel mChannel =
+                    new NotificationChannel(CHANNEL_ID, name, NotificationManager.IMPORTANCE_DEFAULT);
+
+            // Set the Notification Channel for the Notification Manager.
+            mNotificationManager.createNotificationChannel(mChannel);
+        }
+        requestLocationUpdates();
+    }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-//        Log.d("gps", "알람이울려야함");
-//        Log.d("gps", intent.getStringExtra("uid"));
-//        Log.d("gps", intent.getStringExtra("roomId"));
         String roomId = intent.getStringExtra("roomId");
         String uid = intent.getStringExtra("uid");
         userRef = FirebaseDatabase.getInstance().getReference("rooms").child(roomId).child("users").child(uid);
 
+        Log.i(TAG, "Service started");
+        startForeground(NOTIFICATION_ID, getNotification());
+        boolean startedFromNotification = intent.getBooleanExtra(EXTRA_STARTED_FROM_NOTIFICATION,
+                false);
 
-        //노티피케이션 클릭하면 나오는 액티비티 설정
-        Intent intent1 = new Intent(this, MapActivity.class);
-        intent1.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent1, PendingIntent.FLAG_UPDATE_CURRENT);
-
-
-        //Foreground 에서 실행되면 Notification을 보여줘야 됨
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            RemoteViews remoteVIews = new RemoteViews(getPackageName(), R.layout.notification_foreground);
-
-            //Oreo(26)버전 이후 버전부터는 channel이 필요함
-            String channelId = createNotificationChannel();
-
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channelId);
-            Notification notification = builder.setOngoing(true).setSmallIcon(R.mipmap.ic_launcher).setContent(remoteVIews)
-//                    .setCategory(Notification.CATEGORY_SERVICE);
-                    .setContentIntent(pendingIntent)
-                    .build();
-
-            startForeground(1, notification);
+        // We got here because the user decided to remove location updates from the notification.
+        if (startedFromNotification) {
+            removeLocationUpdates();
+            stopSelf();
         }
-
-        //내 위치를  파이어베이스에 업데이트 해보자
-        final GpsTracker tracker = new GpsTracker(this);
-
-//        Log.d("gps", "3초대기");
-
-
-
-
-        TimerTask task = new TimerTask() {
-            @Override
-            public void run() {
-//                Log.d("gps", "lat=" + tracker.getLatitude());
-                userRef.child("lat").setValue(Double.toString(tracker.getLatitude()));
-//                Log.d("gps", "lon=" + tracker.getLongitude());
-                userRef.child("lon").setValue(Double.toString(tracker.getLongitude()));
-
-            }
-        };
-
-        Timer timer = new Timer();
-        long delay = 3;
-        long inteval = 10*1000;
-        timer.scheduleAtFixedRate(task,delay,inteval);
-
-
+        // Tells the system to not try to recreate the service after it has been killed.
         return START_NOT_STICKY;
     }
 
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mChangingConfiguration = true;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // Called when a client (MainActivity in case of this sample) comes to the foreground
+        // and binds with this service. The service should cease to be a foreground service
+        // when that happens.
+        Log.i(TAG, "in onBind()");
+        stopForeground(true);
+        mChangingConfiguration = false;
+        return mBinder;
+    }
+
+    @Override
+    public void onRebind(Intent intent) {
+        // Called when a client (MainActivity in case of this sample) returns to the foreground
+        // and binds once again with this service. The service should cease to be a foreground
+        // service when that happens.
+        Log.i(TAG, "in onRebind()");
+        stopForeground(true);
+        mChangingConfiguration = false;
+        super.onRebind(intent);
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.i(TAG, "Last client unbound from service");
+
+        // Called when the last client (MainActivity in case of this sample) unbinds from this
+        // service. If this method is called due to a configuration change in MainActivity, we
+        // do nothing. Otherwise, we make this service a foreground service.
+        if (!mChangingConfiguration && Utils.requestingLocationUpdates(this)) {
+            Log.i(TAG, "Starting foreground service");
+
+            startForeground(NOTIFICATION_ID, getNotification());
+        }
+        return true; // Ensures onRebind() is called when a client re-binds.
+    }
 
     @Override
     public void onDestroy() {
-        Log.d("AlarmService","Alarm종료");
+        mServiceHandler.removeCallbacksAndMessages(null);
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    private String createNotificationChannel() {
-        String channelId="Alarm";
-        String channelName = getString(R.string.app_name);
-        NotificationChannel channel = new NotificationChannel(channelId,channelName, NotificationManager.IMPORTANCE_NONE);
-//        channel.setDescription(channelName);
-        channel.setSound(null,null);
-        channel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
-        NotificationManager manager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
-        manager.createNotificationChannel(channel);
-
-        return channelId;
+    /**
+     * Makes a request for location updates. Note that in this sample we merely log the
+     * {@link SecurityException}.
+     */
+    public void requestLocationUpdates() {
+        Log.i(TAG, "Requesting location updates");
+        Utils.setRequestingLocationUpdates(this, true);
+//        startService(new Intent(getApplicationContext(), AlarmService.class));
+        try {
+            mFusedLocationClient.requestLocationUpdates(mLocationRequest,
+                    mLocationCallback, Looper.myLooper());
+        } catch (SecurityException unlikely) {
+            Utils.setRequestingLocationUpdates(this, false);
+            Log.e(TAG, "Lost location permission. Could not request updates. " + unlikely);
+        }
     }
 
+    /**
+     * Removes location updates. Note that in this sample we merely log the
+     * {@link SecurityException}.
+     */
+    public void removeLocationUpdates() {
+        Log.i(TAG, "Removing location updates");
+        try {
+            mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+            Utils.setRequestingLocationUpdates(this, false);
+            stopSelf();
+        } catch (SecurityException unlikely) {
+            Utils.setRequestingLocationUpdates(this, true);
+            Log.e(TAG, "Lost location permission. Could not remove updates. " + unlikely);
+        }
+    }
 
+    /**
+     * Returns the {@link NotificationCompat} used as part of the foreground service.
+     */
+    private Notification getNotification() {
+        Intent intent = new Intent(this, AlarmService.class);
 
+        CharSequence text = Utils.getLocationText(mLocation);
+
+        // Extra to help us figure out if we arrived in onStartCommand via the notification or not.
+        intent.putExtra(EXTRA_STARTED_FROM_NOTIFICATION, true);
+
+        // The PendingIntent that leads to a call to onStartCommand() in this service.
+        PendingIntent servicePendingIntent = PendingIntent.getService(this, 0, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // The PendingIntent to launch activity.
+        PendingIntent activityPendingIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, MainActivity.class), 0);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .addAction(R.drawable.ic_launch, getString(R.string.launch_activity),
+                        activityPendingIntent)
+                .addAction(R.drawable.ic_cancel, getString(R.string.remove_location_updates),
+                        servicePendingIntent)
+                .setContentText(text)
+                .setContentTitle(Utils.getLocationTitle(this))
+                .setOngoing(true)
+                .setPriority(Notification.PRIORITY_HIGH)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setTicker(text)
+                .setWhen(System.currentTimeMillis());
+
+        // Set the Channel ID for Android O.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            builder.setChannelId(CHANNEL_ID); // Channel ID
+        }
+
+        return builder.build();
+    }
+
+    private void getLastLocation() {
+        try {
+            mFusedLocationClient.getLastLocation()
+                    .addOnCompleteListener(new OnCompleteListener<Location>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Location> task) {
+                            if (task.isSuccessful()&& task.getResult() != null ) {
+                                mLocation = task.getResult();
+                            } else {
+                                Log.w(TAG, "Failed to get location.");
+                            }
+                        }
+                    });
+        } catch (SecurityException unlikely) {
+            Log.e(TAG, "Lost location permission." + unlikely);
+        }
+    }
+
+    private void onNewLocation(Location location) {
+        Log.i(TAG, "New location: " + location);
+        userRef.child("lat").setValue(Double.toString(location.getLatitude()));
+        userRef.child("lon").setValue(Double.toString(location.getLongitude()));
+
+        mLocation = location;
+
+        // Notify anyone listening for broadcasts about the new location.
+        Intent intent = new Intent(ACTION_BROADCAST);
+        intent.putExtra(EXTRA_LOCATION, location);
+        LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(intent);
+
+        // Update notification content if running as a foreground service.
+        if (serviceIsRunningInForeground(this)) {
+            mNotificationManager.notify(NOTIFICATION_ID, getNotification());
+        }
+    }
+
+    /**
+     * Sets the location request parameters.
+     */
+    private void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
+        mLocationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    /**
+     * Class used for the client Binder.  Since this service runs in the same process as its
+     * clients, we don't need to deal with IPC.
+     */
+    public class LocalBinder extends Binder {
+        AlarmService getService() {
+            return AlarmService.this;
+        }
+    }
+
+    /**
+     * Returns true if this is a foreground service.
+     *
+     * @param context The {@link Context}.
+     */
+    public boolean serviceIsRunningInForeground(Context context) {
+        ActivityManager manager = (ActivityManager) context.getSystemService(
+                Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(
+                Integer.MAX_VALUE)) {
+            if (getClass().getName().equals(service.service.getClassName())) {
+                if (service.foreground) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
-
--- AndroidStudioProjects/MeetingRound/app/src/main/java/com/mgmg/meetinground/MainActivity.java
+++ AndroidStudioProjects/MeetingRound/app/src/main/java/com/mgmg/meetinground/MainActivity.java
@@ -1,14 +1,18 @@
 package com.mgmg.meetinground;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.kakao.auth.ISessionCallback;
 import com.kakao.auth.Session;
 import com.kakao.kakaotalk.callback.TalkResponseCallback;
@@ -25,6 +29,9 @@ public class MainActivity extends Activity {
 
     Uri uri;
     String uid, name, profile, roomId, roomName;
+
+    // Used in checking for runtime permissions.
+    private static final int REQUEST_PERMISSIONS_REQUEST_CODE = 34;
 
     // 세션 콜백 구현
     private ISessionCallback sessionCallback = new ISessionCallback() {
@@ -113,6 +120,14 @@ public class MainActivity extends Activity {
         if (!Session.getCurrentSession().checkAndImplicitOpen()) {
             button.setVisibility(View.VISIBLE);
         }
+
+
+        // Check that the user hasn't revoked permissions by going to Settings.
+        if (Utils.requestingLocationUpdates(this)) {
+            if (!checkPermissions()) {
+                requestPermissions();
+            }
+        }
     }
 
     @Override
@@ -132,5 +147,43 @@ public class MainActivity extends Activity {
         }
 
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private boolean checkPermissions() {
+        return  PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+    }
+    private void requestPermissions() {
+        boolean shouldProvideRationale =
+                ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.ACCESS_FINE_LOCATION);
+
+        // Provide an additional rationale to the user. This would happen if the user denied the
+        // request previously, but didn't check the "Don't ask again" checkbox.
+        if (shouldProvideRationale) {
+            Log.i("MainActivity", "Displaying permission rationale to provide additional context.");
+            Snackbar.make(
+                    findViewById(R.id.activity_main),
+                    R.string.permission_rationale,
+                    Snackbar.LENGTH_INDEFINITE)
+                    .setAction(R.string.ok, new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            // Request permission
+                            ActivityCompat.requestPermissions(MainActivity.this,
+                                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                    REQUEST_PERMISSIONS_REQUEST_CODE);
+                        }
+                    })
+                    .show();
+        } else {
+            Log.i("MainActivity", "Requesting permission");
+            // Request permission. It's possible this can be auto answered if device policy
+            // sets the permission in a given state or the user denied the permission
+            // previously and checked "Never ask again".
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_PERMISSIONS_REQUEST_CODE);
+        }
     }
 }
