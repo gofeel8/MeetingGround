7a8ff228f3be25eefb5cf27ca1440de5d3de8385
박진용 <jypark5826@gmail.com>
2020-09-23 17:04:54 +0900

    [Android/JYP] 커밋 되돌림

--- AndroidStudioProjects/MeetingRound/app/src/main/java/com/mgmg/meetinground/MapActivity.java
+++ AndroidStudioProjects/MeetingRound/app/src/main/java/com/mgmg/meetinground/MapActivity.java
@@ -78,6 +78,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.maps.android.SphericalUtil;
+import com.koushikdutta.ion.Ion;
 
 import java.io.IOException;
 import java.io.InputStream;
