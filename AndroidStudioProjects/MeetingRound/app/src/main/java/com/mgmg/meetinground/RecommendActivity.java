16b5caa0db4a1c31b9380b02ae74b30716227757
박진용 <jypark5826@gmail.com>
2020-09-25 15:17:58 +0900

    [Android/JYP] Volley 이용한 POST method 구현
    Former-commit-id: bac3f24a991d14d92906e069106780906e9743b7

--- AndroidStudioProjects/MeetingRound/app/src/main/java/com/mgmg/meetinground/RecommendActivity.java
+++ AndroidStudioProjects/MeetingRound/app/src/main/java/com/mgmg/meetinground/RecommendActivity.java
@@ -1,181 +1,69 @@
 package com.mgmg.meetinground;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class RecommendActivity extends AppCompatActivity {
-    List<String> navList, locList, menuList, keyList, voteList;
-//    List<String> resultList;
-    StringAdapter navAdapter, locAdapter;
-//    StringAdapter resultAdapter;
-    ListView lvNav, lvLoc, lvMenu, lvKey, lvVote, lvResult;
-    ListView[] listViews;
-    FrameLayout flNav;
-    HashSet<String> navSet, locSet;
-    int height, divider, width, idx;
-
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        height = lvNav.getHeight();
-        divider = lvNav.getDividerHeight();
-        width = 315;
-        idx = -1;
-
-        navAdapter = new StringAdapter(getApplicationContext(), navList, height, divider, navSet);
-        locAdapter = new StringAdapter(getApplicationContext(), locList, height, divider, locSet);
-
-        lvNav.setAdapter(navAdapter);
-        lvLoc.setAdapter(locAdapter);
-
-
-
-
-        lvNav.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if (position == idx) {
-                    listViews[idx].setVisibility(View.INVISIBLE);
-                    idx = -1;
-                    flNav.setLayoutParams(new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.MATCH_PARENT));
-                    navSet.clear();
-                    navAdapter.notifyDataSetChanged();
-
-                } else {
-                    if (idx != -1) {
-                        listViews[idx].setVisibility(View.INVISIBLE);
-                    }
-                    listViews[position].setVisibility(View.VISIBLE);
-                    idx = position;
-
-                    if (idx == 3) {
-                        flNav.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-                    } else {
-                        flNav.setLayoutParams(new LinearLayout.LayoutParams(width, ViewGroup.LayoutParams.MATCH_PARENT));
-                    }
-
-                    navSet.clear();
-                    navSet.add(navList.get(position));
-                    navAdapter.notifyDataSetChanged();
-                }
-            }
-        });
-
-        lvLoc.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String item = locList.get(position);
-                if (locSet.contains(item)) {
-                    locSet.remove(item);
-                } else {
-                    locSet.add(item);
-                }
-                locAdapter.notifyDataSetChanged();
-            }
-        });
-    }
+    StringAdapter keyAdapter;
+    ListView lvKey, lvResult;
+    String[] keys = {"존맛","혼밥","갬성","깔끔","백종원","가성비","채식","한식","양식","분식","면류","치킨","피자","중식","카페","고기집","디저트","샌드위치","버거","주점","도시락","뷔페","기타"};
+    boolean[] checked;
+    double lat, lon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommend);
 
-        lvNav = findViewById(R.id.lvNav);
-        listViews = new ListView[4];
-        lvLoc = findViewById(R.id.lvLoc);
-        listViews[0] = lvLoc;
-        lvMenu = findViewById(R.id.lvMenu);
-        listViews[1] = lvMenu;
         lvKey = findViewById(R.id.lvKey);
-        listViews[2] = lvKey;
-        lvVote = findViewById(R.id.lvVote);
-        listViews[3] = lvVote;
-
-        flNav = findViewById(R.id.flNav);
         lvResult = findViewById(R.id.lvResult);
 
-        navSet = new HashSet<>();
-        locSet = new HashSet<>();
+        Intent intent = getIntent();
+        lat = intent.getDoubleExtra("lat", 0);
+        lon = intent.getDoubleExtra("lon", 0);
 
-        navList = new ArrayList<>();
-        navList.add("지역");
-        navList.add("메뉴");
-        navList.add("키워드");
-        navList.add("투표함");
+        checked = new boolean[keys.length];
+        keyAdapter = new StringAdapter(getApplicationContext(), keys, checked);
+        lvKey.setAdapter(keyAdapter);
 
-        locList = new ArrayList<>();
-        locList.add("서울특별시");
-        locList.add("부산광역시");
-        locList.add("대구광역시");
-        locList.add("인천광역시");
-        locList.add("광주광역시");
-        locList.add("대전광역시");
-        locList.add("울산광역시");
-        locList.add("세종특별자치시");
-        locList.add("경기도");
-        locList.add("강원도");
-        locList.add("충청북도");
-        locList.add("충청남도");
-        locList.add("전라북도");
-        locList.add("전라남도");
-        locList.add("경상북도");
-        locList.add("경상남도");
-        locList.add("제주특별자치도");
+        lvKey.setOnItemClickListener((parent, view, position, id) -> {
+            checked[position] = !checked[position];
 
-        BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
-        bottomNavigation.setSelectedItemId(R.id.tab3);
-        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Intent intent = getIntent();
-                switch (item.getItemId()){
-                    case R.id.tab1:
-                        intent = getIntent();
-                        Intent intent2=new Intent(getApplicationContext(),RoomActivity.class);
-                        intent2.putExtras(intent);
-                        intent2.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                        startActivity(intent2);
-                        return true;
-                    case R.id.tab2:
-                        intent = getIntent();
-                        Intent intent3=new Intent(getApplicationContext(),MapActivity.class);
-                        intent3.putExtras(intent);
-                        intent3.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                        startActivity(intent3);
-                        return true;
-                    case R.id.tab3:
-                        return true;
-                }
-                return false;
+            try {
+                // 아이템이 클릭될 때마다 비동기 요청 전송
+                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+                String url = "http://j3b207.p.ssafy.io:8080/api/search";
+                JSONObject obj = new JSONObject();
+                obj.put("lat", lat);
+                obj.put("lon", lon);
+                JSONArray arr = new JSONArray(checked);
+                obj.put("keys", arr);
+
+                // Add the request to the RequestQueue.
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, obj, response -> System.out.println(response), error -> System.out.println(error));
+                queue.add(jsonObjectRequest);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
-        });
-    }
 
-    @Override
-    protected void onPostResume() {
-        super.onPostResume();
-        BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
-        bottomNavigation.setSelectedItemId(R.id.tab3);
-    }
-    @Override
-    protected void onPause() {
-        super.onPause();
-        overridePendingTransition(0, 0);
+            keyAdapter.notifyDataSetChanged();
+        });
     }
 }
