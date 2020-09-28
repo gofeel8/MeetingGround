f4e880e69916e37850b7cd81ae9b8e9bb7e064d7
unknown <kimmydkemf@naver.com>
2020-09-28 15:57:56 +0900

    [Android/LSH] 카메라 위치변경 및 게임시작/회원은 장소 설정 변경 제어
    Former-commit-id: a826bc5ad930ffb03ab15bc6f4bb1a8755ff0442

--- AndroidStudioProjects/MeetingRound/app/src/main/java/com/mgmg/meetinground/MapActivity.java
+++ AndroidStudioProjects/MeetingRound/app/src/main/java/com/mgmg/meetinground/MapActivity.java
@@ -34,6 +34,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -113,6 +114,8 @@ public class MapActivity  extends AppCompatActivity implements OnMapReadyCallbac
     private Long meetingTime;
     private String address;
     private String api_key="AIzaSyAMy-qeSOF-mrR6_aLzMDGc9YgsW70UCfQ";
+    static boolean first=false;
+    private LinearLayout container;
 
     private static final int GPS_ENABLE_REQUEST_CODE = 2001;
     private static final int PERMISSIONS_REQUEST_CODE = 100;
@@ -143,7 +146,13 @@ public class MapActivity  extends AppCompatActivity implements OnMapReadyCallbac
         roomId=intent.getStringExtra("roomId");
         uid=intent.getStringExtra("uid");
 
+        container=findViewById(R.id.selection);
+
         database = FirebaseDatabase.getInstance().getReference();
+
+        if(database.child("rooms").child(roomId).child("info").child("users").child(uid).child("host").getKey()==null){
+            container.removeAllViews();
+        }
 
         editText=(EditText)findViewById(R.id.editText);
 
@@ -163,7 +172,7 @@ public class MapActivity  extends AppCompatActivity implements OnMapReadyCallbac
             }
         });
 
-        database.child("rooms").child(roomId).child("info").child("users").child(uid).addValueEventListener(new ValueEventListener(){
+        database.child("rooms").child(roomId).child("info").child("users").child(uid).child("lat").addValueEventListener(new ValueEventListener(){
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 LatLng now=new LatLng(gpsTracker.getLatitude(), gpsTracker.getLongitude());
@@ -247,6 +256,7 @@ public class MapActivity  extends AppCompatActivity implements OnMapReadyCallbac
                         return;
                     }
                     GameStart=true;
+                    container.removeAllViews();
                     // 2. 각자의 위치를 띄운다.
 //                    List<LatLng> position=new LinkedList<>();
                     List<LatLng> user_pos=new LinkedList<>();
@@ -717,17 +727,20 @@ public class MapActivity  extends AppCompatActivity implements OnMapReadyCallbac
                     markerOptions.icon(BitmapDescriptorFactory.fromBitmap(getCircularBitmap(smallMarker)));
                 }
                 mMap.addMarker(markerOptions);
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(mine));
-                mMap.animateCamera(CameraUpdateFactory.zoomTo(12));
+                if(!GameStart) {
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(mine));
+                    mMap.animateCamera(CameraUpdateFactory.zoomTo(12));
+                }
                 if(position==null)
                     return;
                 markerOptions.position(position);
                 markerOptions.title("meeting place");
                 markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
                 mMap.addMarker(markerOptions);
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(position));
-                mMap.animateCamera(CameraUpdateFactory.zoomTo(12));
-
+                if(!GameStart) {
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(position));
+                    mMap.animateCamera(CameraUpdateFactory.zoomTo(12));
+                }
                 if(users==null) {
                     return;
                 }
@@ -757,14 +770,17 @@ public class MapActivity  extends AppCompatActivity implements OnMapReadyCallbac
                 com.mgmg.meetinground.distance Distance=new com.mgmg.meetinground.distance("now",SphericalUtil.computeDistanceBetween(now,position));
 
                 circlesize=(int)(meetingTime-System.currentTimeMillis())/10;
-                if(circlesize>0) {
-                    mMap.addCircle(new CircleOptions()
-                            .center(position)
-                            .radius(circlesize)
-                            .fillColor(Color.parseColor("#50F08080"))); // in meters
+                if(circlesize<100)
+                    circlesize=100;
+                mMap.addCircle(new CircleOptions()
+                        .center(position)
+                        .radius(circlesize)
+                        .fillColor(Color.parseColor("#50F08080"))); // in meters
+                if(!first){
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(position));
+                    mMap.animateCamera(CameraUpdateFactory.zoomTo(12));
+                    first=true;
                 }
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(position));
-                mMap.animateCamera(CameraUpdateFactory.zoomTo(12));
             }
         };
         handler.post(run);
