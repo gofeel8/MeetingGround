de29fa71ad539f17b66e786eab9f6ddcb0be8059
philsang <gofeel8@hanmail.net>
2020-09-29 13:13:51 +0900

    [Android/gps]레스토링다테일수정
    Former-commit-id: 4eb70954c0627c1cc4f31fa2a00f2783d435f7c5

--- AndroidStudioProjects/MeetingRound/app/src/main/java/com/mgmg/meetinground/RestaurantDetail.java
+++ AndroidStudioProjects/MeetingRound/app/src/main/java/com/mgmg/meetinground/RestaurantDetail.java
@@ -3,19 +3,25 @@ package com.mgmg.meetinground;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class RestaurantDetail extends AppCompatActivity {
+public class RestaurantDetail extends AppCompatActivity implements OnMapReadyCallback {
     private DatabaseReference database;
+    private GoogleMap mMap;
+    Restaurant restaurant;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,8 +30,12 @@ public class RestaurantDetail extends AppCompatActivity {
 
         TextView name;
         TextView tag;
-        ImageView imgView;
-        Button like,hate;
+        ImageView imgView,like,hate;
+
+        SupportMapFragment mapFragment=(SupportMapFragment)getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
 
         name = findViewById(R.id.tv_name);
         tag = findViewById(R.id.tv_tag);
@@ -34,10 +44,25 @@ public class RestaurantDetail extends AppCompatActivity {
         hate = findViewById(R.id.btn_hate);
 
 
-        Restaurant restaurant = null;
+        restaurant = null;
         Intent intent=getIntent();
         restaurant =  (Restaurant) intent.getParcelableExtra("obj");
-        Toast.makeText(this, restaurant.getName(), Toast.LENGTH_SHORT).show();
+
+
+        if(restaurant != null){
+            Toast.makeText(this, restaurant.getName(), Toast.LENGTH_SHORT).show();
+            name.setText(restaurant.getName());
+            if(restaurant.getTags() !=null){
+                tag.setText(restaurant.getTags().toString());
+            }
+            LatLng rest = new LatLng(Double.parseDouble(restaurant.getLat()), 	Double.parseDouble(restaurant.getLon()));
+            mMap.addMarker(new MarkerOptions()
+                    .position(rest)
+                    .title(restaurant.getName()));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(rest));
+        }else{
+            Toast.makeText(this,"레스토랑이비었습니다", Toast.LENGTH_SHORT).show();
+        }
 
 
 
@@ -47,5 +72,18 @@ public class RestaurantDetail extends AppCompatActivity {
 
 
 
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+
+        LatLng rest = new LatLng(37.413294, 	126.734086);
+//        mMap.addMarker(new MarkerOptions()
+//                .position(rest)
+//                .title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(rest));
     }
 }
